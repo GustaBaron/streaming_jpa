e5b2dd8bee14596822a98b9309af8d6ad14dac07
1144007836 <gustavoalecxander9@gmail.com>
2025-09-22 21:27:27 -0300

    add values to class avaliacao and categoria

--- src/main/java/com/univille/jpa/streaming/avaliacao.java
+++ src/main/java/com/univille/jpa/streaming/avaliacao.java
@@ -1,4 +1,70 @@
 package com.univille.jpa.streaming;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
+import java.time.LocalDateTime;
+
+@Entity
 public class avaliacao {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+    private String nome;
+    private String email;
+    private String telefone;
+    private LocalDateTime data;
+
+    public avaliacao() {
+
+    }
+
+    public avaliacao(Long id, String nome, String email, String telefone, LocalDateTime data) {
+        Id = id;
+        this.nome = nome;
+        this.email = email;
+        this.telefone = telefone;
+        this.data = data;
+    }
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public LocalDateTime getData() {
+        return data;
+    }
+
+    public void setData(LocalDateTime data) {
+        this.data = data;
+    }
 }
--- src/main/java/com/univille/jpa/streaming/categoria.java
+++ src/main/java/com/univille/jpa/streaming/categoria.java
@@ -1,4 +1,41 @@
 package com.univille.jpa.streaming;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class categoria {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String nome;
+
+    public categoria(int id) {
+        this.id = id;
+    }
+
+    public categoria(int id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }
+
