928f21e9e8de15ad92423c1a8ee4e68480bd7088
Gustavo Baron <gustavoalecxander9@gmail.com>
2025-09-24 17:56:33 -0300

    começando base para lista

--- src/main/java/com/univille/jpa/streaming/StreamingApplication.java
+++ src/main/java/com/univille/jpa/streaming/StreamingApplication.java
@@ -1,7 +1,11 @@
 package com.univille.jpa.streaming;
 
+import com.univille.jpa.streaming.Repository.*;
+import com.univille.jpa.streaming.entity.avaliacao;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class StreamingApplication {
@@ -9,5 +13,19 @@ public class StreamingApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(StreamingApplication.class, args);
 	}
+
+    @Bean
+    CommandLineRunner init(avaliacaoRepository avaRepo, categoriaRepository catRepo, perfilRepository perRepo ,
+                           usuarioRepository usuRepo, videoRepository vidRepo , visualizacaoRepository visRepo) {
+        return args -> {
+            // Inserções
+            //Id é nulo -> insere o registro ( insert )
+            // Id não é nulo -> atualiza o registro ( update )
+            avaRepo.save(new avaliacao());
+
+            System.out.println("Lista de Avaliação");
+            avaRepo.findAll().forEach(System.out::println);
+        };
+    }
 
 }
